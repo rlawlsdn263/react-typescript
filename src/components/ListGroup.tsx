d28cc1cc95510a362fbbebc0c32c4447cec12a2e
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-30 17:38:19 +0900

    [✍학습] 조건부 렌더링
    - 삼항 연산자 혹은 AND 연산자를 사용하면 조건부로 JSX를 렌더링할 수 있음
    - AND 연산자일 경우, 연산자 앞의 경우가 true면 뒤의 값을 반환하고, false라면 전체가 false를 반환하기 때문에 아무것도 나오지 않음

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,17 @@
 function ListGroup() {
-  const cities = ["뉴욕", "서울", "파리", "도쿄"];
+  let cities = ["뉴욕", "서울", "파리", "도쿄"];
+  cities = [];
+
+  /* const message = cities.length === 0 ? <p>도시 데이터 없음</p> : null;
+
+  function getMessage() {
+    return cities.length === 0 ? <p>도시 데이터 없음</p> : null;
+  } */
 
   return (
     <>
       <h1>세계 주요 관광 도시</h1>
+      {cities.length === 0 && <p>도시 데이터 없음</p>}
       <ul className="list-group">
         {cities.map((item) => (
           <li key={item}>{item}</li>
